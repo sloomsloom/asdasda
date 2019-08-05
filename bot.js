bdac7c08fca8f47c2aabb893cf58c41c041278be
sloomsloom <53345575+sloomsloom@users.noreply.github.com>
2019-08-05 21:11:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
-var adminprefix = '#'
+var adminprefix = '-'
 
 
-const developers = ["552153150883233793"]
+const developers = ["581212333372735606"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
